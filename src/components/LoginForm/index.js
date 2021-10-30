4ff8970d9c69e19d34a855b59d93762d8d5029db
Asadbek13 <69428867+Asadbek13@users.noreply.github.com>
2021-10-30 13:50:26 +0500

    Login page was created and implemented

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,15 +1,51 @@
+import { useState } from "react";
+
 import * as Styled from "./style";
 import CompanyIcon from "../../assets/companyIcon.png";
+import * as Icons from "../../constants/icons";
 
 const LoginForm = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <Styled._ attr="main">
       <Styled._ attr="icon">
         <img src={CompanyIcon} alt="" draggable="false" />
       </Styled._>
-      <Styled._ attr="inputs"></Styled._>
-      <Styled._ attr="options"></Styled._>
-      <Button></Button>
+      <Styled._ attr="inputs">
+        <Styled._ attr="username">
+          <Styled._ attr="inputIcon">
+            <Icons.UserIcon />
+          </Styled._>
+          <Styled.Input type="text" placeholder="example@mail.com" />
+        </Styled._>
+        <Styled._ attr="password">
+          <Styled._ attr="inputIcon">
+            <Icons.LockIcon />
+          </Styled._>
+          <Styled.Input
+            type={showPassword ? "text" : "password"}
+            placeholder="password"
+          />
+          <Styled._
+            attr="inputIcon"
+            attr2="asButton"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            <Icons.EyeIcon />
+          </Styled._>
+        </Styled._>
+      </Styled._>
+      <Styled._ attr="options">
+        <Styled._ attr="checkbox">
+          <Styled.Checkbox type="checkbox" id="rememberMe" />
+          <Styled.Label attr="remember" for="rememberMe">
+            Remember me
+          </Styled.Label>
+        </Styled._>
+        <p>Forgot password?</p>
+      </Styled._>
+      <Styled.Button>Login</Styled.Button>
     </Styled._>
   );
 };
--- src/components/LoginForm/style.js
+++ src/components/LoginForm/style.js
@@ -1,3 +1,101 @@
 import styled, { css } from "styled-components";
 
-export const _ = styled.div``;
+export const _ = styled.div`
+  ${(props) =>
+    ({
+      main: css`
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+      `,
+      icon: css`
+        margin-bottom: 80px;
+      `,
+      options: css`
+        user-select: none;
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        margin-top: 30px;
+        color: #2e3a59;
+      `,
+      checkbox: css`
+        display: flex;
+        align-items: center;
+
+        & > * {
+          cursor: pointer;
+        }
+        & > label {
+          margin-left: 5px;
+        }
+      `,
+      inputs: css`
+        display: flex;
+        flex-direction: column;
+        gap: 30px;
+      `,
+      username: css`
+        display: flex;
+        overflow: hidden;
+        height: 40px;
+        width: 350px;
+        border-radius: 5px;
+        background-color: white;
+      `,
+      password: css`
+        display: flex;
+        overflow: hidden;
+        height: 40px;
+        width: 350px;
+        border-radius: 5px;
+        background-color: white;
+      `,
+      inputIcon: css`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        transition: background-color 0.2s;
+      `,
+    }[props.attr])}
+
+  ${(props) =>
+    ({
+      asButton: css`
+        cursor: pointer;
+        &:hover {
+          background-color: rgba(0, 0, 0, 0.1);
+        }
+      `,
+    }[props.attr2])}
+`;
+
+export const Button = styled.button`
+  width: 150px;
+  height: 40px;
+  margin-top: 30px;
+  border: none;
+  border-radius: 5px;
+  font-size: 14px;
+  letter-spacing: 2px;
+  color: white;
+  background-color: #576c89;
+  transition: opacity 0.2s;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  font-size: 16px;
+`;
+
+export const Checkbox = styled.input``;
+
+export const Label = styled.label``;
