2b661a738dcdf245ed6e1089e2b68613db5a3b0b
dinainamdzanova <dinainamdzanova@gmail.com>
2021-11-28 15:07:11 +0500

    Card components has been created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import Login from "./pages/Login";
-
+import * as Styled from "./components/dashboard/card";
+import * as Global from "./components/global";
 function App() {
   return (
-    <div>
-      <Login />
-    </div>
+    <Global.Grid gap="30" columns="4">
+      <Styled.Card>
+        <Styled.CardTitle>
+          <Global.Flexbox align="center">
+            <Global.Text color="#ffffff70;" fontSize="20px">
+              Students:
+            </Global.Text>
+            <Global.Text color="#ffffff;" fontSize="20px">
+              30
+            </Global.Text>
+          </Global.Flexbox>
+          <Styled.CardBadge>30</Styled.CardBadge>
+        </Styled.CardTitle>
+        <Styled.CardBody>
+          <Global.Flexbox
+            margin="2.5px 0"
+            align="center"
+            justify="space-between"
+          >
+            <Global.Text color="#14274E;" fontSize="20">
+              Subject
+            </Global.Text>
+            <Global.Badge>English101</Global.Badge>
+          </Global.Flexbox>
+          <Global.Flexbox
+            margin="2.5px 0"
+            align="center"
+            justify="space-between"
+          >
+            <Global.Text color="#14274E;" fontSize="20">
+              Subject
+            </Global.Text>
+            <Global.Badge>English101</Global.Badge>
+          </Global.Flexbox>
+          <Global.Flexbox
+            margin="2.5px 0"
+            align="center"
+            justify="space-between"
+          >
+            <Global.Text color="#14274E;" fontSize="20">
+              Subject
+            </Global.Text>
+            <Global.Badge>English101</Global.Badge>
+          </Global.Flexbox>
+        </Styled.CardBody>
+      </Styled.Card>
+    </Global.Grid>
   );
 }
 
